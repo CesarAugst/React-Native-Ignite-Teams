6ed62f4c8c79693771639c0eaff7c6b7d3eba4bf
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-12-07 10:07:23 -0300

    hook para renderizar lista usando FlatList. Componente de lista vazia

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,8 +2,12 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
 import { GroupCard } from '@components/GroupCards';
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header/>
@@ -11,8 +15,21 @@ export function Groups() {
         title="Turmas"
         subtitle='Jogue com a sua turma'  
       />
-
-      <GroupCard title="Galera do Ignite"/>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1}}
+        ListEmptyComponent={() => (
+          <ListEmpty 
+            message="Que tal cadastrar a primeira turma?"
+          />
+        )}
+      />
     </Container>
   );
 }
