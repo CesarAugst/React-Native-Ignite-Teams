0473b168668c114ba6c3d038534b505972f68242
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-12-07 11:29:26 -0300

    input

--- src/screens/NewGroups/index.tsx
+++ src/screens/NewGroups/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroups() {
@@ -13,8 +14,12 @@ export function NewGroups() {
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
+        <Input
+          placeholder="Nome da turma"
+        />
         <Button
           title="Criar"
+          style={{ marginTop: 20}}
         />
       </Content>
     </Container>
